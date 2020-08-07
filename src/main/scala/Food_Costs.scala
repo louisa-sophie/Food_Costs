ddfc9b4fc76a47a13d53165b1cff6d6feb38e5fb
louisa-sophie <louisa_sophie_1@web.de>
2020-08-07 09:31:54 +0300

    Initial commit

---
+++ src/main/scala/Food_Costs.scala
@@ -0,0 +1,20 @@
+object Food_Costs extends App {
+
+  println("Food Costs Calculation")
+
+  val food = "potatoes"
+  val price = 0.37
+  val quantity = 20
+
+val costs = price*quantity
+  println(s"Costs: $costs")
+
+  println(s"It will cost $costs € to buy $quantity kilos of $food.")
+
+
+
+  //println how much will it cost to buy the food
+  //It will cost XXXX to buy XX kilos of XXXX
+  //println("It will cost XXXX to buy XX kilos of XXXX")
+
+}
